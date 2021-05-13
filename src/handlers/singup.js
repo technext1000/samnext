b3a0b4f2cecfa4da2ef9efd90b066a86f9c58997
technext1000 <hotasambit336@gmail.com>
2021-05-13 10:24:07 +0530

    changes

---
+++ src/handlers/singup.js
@@ -0,0 +1,30 @@
+const tableName = process.env.SAMPLE_TABLE;
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+const tableName = process.env.Profile;
+exports.signUpFunction=async (event)=>{
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    console.info('received:', event);
+    const body = JSON.parse(event.body)
+
+    var params = {
+        TableName : tableName,
+        Item: { email : body.email, name: body.name ,gender: body.gender,mobile:body.mobile,password: body.password }
+    };
+
+    const result = await docClient.put(params).promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify("Registration Successfuly Comppleted")
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+    
+}
